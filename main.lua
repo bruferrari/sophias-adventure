03e541ccce3357b9b53aba81e3f24a8d62c580b8
Bruno Ferrari <bruno.ferrari@enjoei.com.br>
2022-01-20 09:00:23 -0300

    Add jump mechanic and colision queries

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ wf = require('libs/windfield/windfield')
 
 function love.load()
     world = wf.newWorld(0, 800, false)
+    world:setQueryDebugDrawing(true)
+
+    world:addCollisionClass('platform')
+
     spawnPlatform()
     require('player')
 end
@@ -19,9 +23,15 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+
+    if key == 'up' then
+        if not player.jumping then
+            player:applyLinearImpulse(0, -1500)
+        end
+    end
 end
 
 function spawnPlatform()
-    local platform = world:newRectangleCollider(100, 200, 300, 100, nil)
+    local platform = world:newRectangleCollider(200, 400, 300, 100, { collision_class='platform' })
     platform:setType('static')
 end
--- player.lua
+++ player.lua
@@ -1,9 +1,18 @@
-player = world:newRectangleCollider(360, 100, 40, 100, nil)
+player = world:newRectangleCollider(360, 100, 40, 40, nil)
 player.speed = 200
 player.direction = 1
+player.jumping = false
 
 function playerUpdate(dt)
     if player.body then
+        local colliders = world:queryRectangleArea(
+            player:getX() - 20,
+            player:getY() + 20,
+            40,
+            2,
+            {'platform'}
+        )
+
         local px, py = player:getPosition()
 
         if love.keyboard.isDown('left') then
@@ -14,6 +23,12 @@ function playerUpdate(dt)
         if love.keyboard.isDown('right') then
             player:setX(px + player.speed * dt)
             player.direction = 1
+        end
+
+        if #colliders > 0 then
+            player.jumping = false
+        else
+            player.jumping = true
         end
     end
 end
